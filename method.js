f552906d1ff2bb58bc910ff3b5b4c562f5232397
Zara Khan <zarakhang067@gmail.com>
2025-12-20 18:36:10 -0800

    Add files via upload

---
+++ method.js
@@ -0,0 +1,16 @@
+// const math = require("./export");
+
+// console.log(math.add(5, 1));
+// console.log(math.sub(5, 1));
+// console.log(math.mul(5,1));
+// console.log(math.div(5,1));
+
+
+
+
+
+
+const msg = require("./export");
+
+console.log(msg.hello("Ali"));
+console.log(msg.bye("Ali"));
