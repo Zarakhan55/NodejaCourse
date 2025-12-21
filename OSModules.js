8b5e54257aa229b609f74b6d61d6a805b3152517
Zara Khan <zarakhang067@gmail.com>
2025-12-20 18:59:07 -0800

    Add files via upload

---
+++ OSModules.js
@@ -0,0 +1,18 @@
+const os = require("os");
+
+console.log("OS platform:", os.platform()); 
+// Shows operating system (win32, linux, darwin)
+
+console.log("CPU architecture:", os.arch()); 
+// Shows CPU type (x64, arm)
+
+console.log("Total memory:", os.totalmem()); 
+// Total RAM in bytes
+
+console.log("Free memory:", os.freemem()); 
+// Free RAM in bytes
+
+console.log("Home directory:", os.homedir()); 
+// User home folder path
+
+ 
