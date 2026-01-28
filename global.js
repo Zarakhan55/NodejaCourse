a79b36371dedff95d7da0c44385427b33d52ab35
Zara Khan <zarakhang067@gmail.com>
2026-01-28 09:11:36 -0800

    Add files via upload

---
+++ global.js
@@ -0,0 +1,13 @@
+// console.log(process.argv);
+console.log("Node Version:", process.version);
+console.log("Platform:", process.platform);
+// console.log("Directory:", __dirname);
+// console.log("File:", __filename);
+// Q: What is Node REPL?
+// A: REPL is an interactive shell that allows us to run and test JavaScript code line by line in Node.js.
+
+console.log("Hello");
+console.warn("Warning");
+console.error("Error");
+
+// REPL = Read – Eval – Print – Loop
